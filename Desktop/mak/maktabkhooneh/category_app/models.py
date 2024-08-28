a36b12a49b20b204bdec9840ccbf72bc02575d03
Your Name <you@example.com>
2024-08-28 12:01:47 -0700

    def-str

--- Desktop/mak/maktabkhooneh/category_app/models.py
+++ Desktop/mak/maktabkhooneh/category_app/models.py
@@ -3,5 +3,7 @@ from django.db import models
 
 class Category(models.Model) :
     name = models.CharField( default="" ,  max_length=50)
+    def __str__(self) -> str:
+        return self.name
     # clas = models.ForeignKey(Clas , verbose_name= "class"  , on_delete=models.CASCADE )
 # Create your models here.
