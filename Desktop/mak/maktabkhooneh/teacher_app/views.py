e3c2e2000063d40d6630ddd580dd74ae2418d192
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-04 21:59:11 +0430

    adding reacher_app views

--- Desktop/mak/maktabkhooneh/teacher_app/views.py
+++ Desktop/mak/maktabkhooneh/teacher_app/views.py
@@ -1,72 +1,51 @@
-from django.http.response import HttpResponse, JsonResponse
-from teacher_app.models import Teacher
-from django.views.decorators.csrf import csrf_exempt
-import json
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Teacher
+from .serializers import TeacherSerializer
 
+class TeacherList(ListAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
 
-def teacher_list(request):
-    all_teachers = Teacher.objects.all()
-    my_teacher_list = []
-    for teacher in all_teachers:
-        teacher_dictionary = {
-            "first name" : teacher.first_name,
-            "last name" : teacher.last_name,
-            "email" : teacher.email,
-            "phone number " : teacher.phone_number,
-            "clas" : teacher.clas,
-            # "category" : teacher.category,
-            "rate" : teacher.rate,
-            "wallet" : teacher.wallet
-        }
-        my_teacher_list.append(teacher_dictionary)
-    return JsonResponse(my_teacher_list , safe=False)
+class TeacherDetail(RetrieveAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
 
+class CreateTeacher(CreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
 
-@csrf_exempt
-def signup(request) :
-    if request.method == 'POST' :
-        body = json.loads(request.body)
-        Teacher.objects.create(
-            name= body['name_inp'] , 
-            email = body['email_inp'] , 
-            phone_number = body['phone_number_inp'] ,
-        )
-        return HttpResponse("new user added")
-    else : 
-        return HttpResponse("bad request!")
+class UpdateTeacher(UpdateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
 
-    
+class DeleteTeacher(DestroyAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
 
-def average_rate(request):  
-    if request.method == 'GET':  
+class TeacherDetailUpdateDelete(RetrieveUpdateDestroyAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class AverageRateAPIView(ListAPIView):
+    def get(self, request, *args, **kwargs):
         all_teachers = Teacher.objects.all()
-        if not all_teachers:  
-            return JsonResponse({"average_rate": 0})  
+        if not all_teachers:
+            return Response({"average_rate": 0}, status=status.HTTP_200_OK)
         total_rate = sum(teacher.rate for teacher in all_teachers)
-        average = total_rate / len(all_teachers)  
-        return JsonResponse({"average_rate": average})  
-    else:  
-        return HttpResponse("bad request!", status=400)
+        average = total_rate / len(all_teachers)
+        return Response({"average_rate": average}, status=status.HTTP_200_OK)
 
-
-def sort_by_rate(request):
-    if request.method == 'GET':
-        all_teachers = Teacher.objects.all()
-        sorted_list = sorted(all_teachers, key=lambda teacher: teacher.rate, reverse=True)
-        my_teacher_list = []
-        for teacher in sorted_list:
-            teacher_dictionary = {
-                "first name" : teacher.first_name,
-                "last name" : teacher.last_name,
-                "email" : teacher.email,
-                "phone number " : teacher.phone_number,
-                "clas" : teacher.clas,
-                # "category" : teacher.category,
-                "rate" : teacher.rate,
-                "wallet" : teacher.wallet
-            }
-            my_teacher_list.append(teacher_dictionary)
-        return JsonResponse(my_teacher_list , safe=False)
-    else:
-        return HttpResponse("bad request!")
-        
+class SortByRateAPIView(ListAPIView):
+    def get(self, request, *args, **kwargs):
+        sorted_list = Teacher.objects.all().order_by('-rate')
+        serializer = TeacherSerializer(sorted_list, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
