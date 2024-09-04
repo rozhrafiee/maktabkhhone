3ffc2547ea64b658747027d450c888c4f6d2e92b
Pariya.Zarei <art0pariya0z@gmail.com>
2024-09-04 22:15:18 +0330

    adding factor-app urls.py

--- Desktop/mak/maktabkhooneh/factor_app/urls.py
+++ Desktop/mak/maktabkhooneh/factor_app/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-from factor_app.views import calculate_price, create_factor
-
-
-urlpatterns = [
-    path("cal-price", calculate_price),
-]
+from django.urls import path from .views import 
+( FactorList, FactorDetail, CreateFactor, UpdateFactor, DeleteFactor, FactorDetailUpdateDelete,
+ CalculatePriceView, CreateFactorCustomView )
+urlpatterns = [ path('factors/', FactorList.as_view(), name='factor-list'),
+               path('factors/<int:pk>/', FactorDetail.as_view(), name='factor-detail'),
+               path('factors/create/', CreateFactor.as_view(), name='factor-create'),
+               path('factors/update/<int:pk>/', UpdateFactor.as_view(), name='factor-update'), 
+               path('factors/delete/<int:pk>/', DeleteFactor.as_view(), name='factor-delete'),
+               path('factors/manage/<int:pk>/', FactorDetailUpdateDelete.as_view(), name='factor-manage'), 
+               path('factors/calculate-price/',
