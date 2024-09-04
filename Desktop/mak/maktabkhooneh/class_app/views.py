a2809154722e7f6b66c6b0e8f4bfed48e84e2cf2
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-04 21:35:02 +0430

    adding class_app views.py

--- Desktop/mak/maktabkhooneh/class_app/views.py
+++ Desktop/mak/maktabkhooneh/class_app/views.py
@@ -1,49 +1,35 @@
-from django.shortcuts import render , HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from class_app.models import Clas
-from teacher_app.models import Teacher
-import json
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView,
+    DestroyAPIView,
+    UpdateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from .models import Clas
+from .serializers import ClasSerializer
+from django.http import HttpResponse
 
+class ClasList(ListAPIView):
+    queryset = Clas.objects.all()
+    serializer_class = ClasSerializer
 
-def class_list(request):
-    clases = Clas.objects.all()
-    class_list  = []
-    for item in clases:
-        class_dict = {
-            "name": item.name,
-            "time" : item.time,
-            'price ': item.price,
-            'category' : item.category
-            
-        }
-        class_list.append(class_dict)
-    return HttpResponse (class_list)
+class ClasDetail(RetrieveAPIView):
+    queryset = Clas.objects.all()
+    serializer_class = ClasSerializer
 
+class CreateClas(CreateAPIView):
+    queryset = Clas.objects.all()
+    serializer_class = ClasSerializer
 
-@csrf_exempt
-def create_class(request):
-    if request.method == 'POST':
-        body = json.loads(request.body)
-        selected_tacher = Teacher.objects.get(id=body['input_cupon'])
-        Clas.objects.create(
-            name= body['input_name'],
-            price= body['input_price'],
-            time = body['input_class'],
-            category= body['input_category'],
-            teacher = selected_tacher
-        )
-        return HttpResponse("new class reserved and factor made")
-    else:
-        return HttpResponse("BAD REQUEST")
-    
+class UpdateClas(UpdateAPIView):
+    queryset = Clas.objects.all()
+    serializer_class = ClasSerializer
 
-@csrf_exempt
-def update_price(request , inp_id):
-    if request.method == 'PATCH':
-        body = json.loads(request.body)
-        item = Clas.objects.get(id = inp_id)
-        item.price = body['input_price']
-        item.save()
-        return HttpResponse("price updated!")
-    else:
-        return HttpResponse("BAD REQUEST")
+class DeleteClas(DestroyAPIView):
+    queryset = Clas.objects.all()
+    serializer_class = ClasSerializer
+
+class ClasDetailUpdateDelete(RetrieveUpdateDestroyAPIView):
+    queryset = Clas.objects.all()
+    serializer_class = ClasSerializer
