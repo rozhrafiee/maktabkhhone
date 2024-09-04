6a4d9537ab0b059a9aa661c28b60e7b86bea8973
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-04 21:17:52 +0430

    factor_app views.py

--- Desktop/mak/maktabkhooneh/factor_app/views.py
+++ Desktop/mak/maktabkhooneh/factor_app/views.py
@@ -1,29 +1,59 @@
-from django.shortcuts import render , HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView,
+    DestroyAPIView,
+    UpdateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from rest_framework.views import APIView
+from .models import Factor
+from .serializers import FactorSerializer
 from cupon_app.models import Cupon
-from factor_app.models import Factor
-import json
+from django.http import HttpResponse
 
+class FactorList(ListAPIView):
+    queryset = Factor.objects.all()
+    serializer_class = FactorSerializer
 
-def calculate_price(request):
-    factors = Factor.objects.all()
-    for items in factors:
-        items.price = items.price - (items.price * items.cupon.percent /100)
-        items.price.save()
+class FactorDetail(RetrieveAPIView):
+    queryset = Factor.objects.all()
+    serializer_class = FactorSerializer
 
-@csrf_exempt
-def create_factor(request):
-    if request.method == 'POST':
-        body = json.loads(request.body)
-        selected_cupon = Cupon.objects.get(id=body['input_cupon'])
-        Factor.objects.create(
-            student_name= body['input_name'],
-            price= body['input_price'],
-            class1 = body['input_class'],
-            cupon = selected_cupon
+class CreateFactor(CreateAPIView):
+    queryset = Factor.objects.all()
+    serializer_class = FactorSerializer
+
+class UpdateFactor(UpdateAPIView):
+    queryset = Factor.objects.all()
+    serializer_class = FactorSerializer
+
+class DeleteFactor(DestroyAPIView):
+    queryset = Factor.objects.all()
+    serializer_class = FactorSerializer
+
+class FactorDetailUpdateDelete(RetrieveUpdateDestroyAPIView):
+    queryset = Factor.objects.all()
+    serializer_class = FactorSerializer
+
+class CalculatePriceView(APIView):
+    def get(self, request):
+        factors = Factor.objects.all()
+        for item in factors:
+            item.price = item.price - (item.price * item.cupon.percent / 100)
+            item.save()
+        return HttpResponse("Prices updated successfully")
+
+class CreateFactorCustomView(APIView):
+    def post(self, request):
+        data = request.data
+        cupon = Cupon.objects.get(id=data['cupon_id'])
+        factor = Factor.objects.create(
+            student_name=data['student_name'],
+            price=data['price'],
+            class1_id=data['class_id'],
+            cupon=cupon,
+            payment_status=data['payment_status']
         )
-        return HttpResponse("new class reserved and factor made")
-    else:
-        return HttpResponse("BAD REQUEST")
-        
-# Create your views here.
+        factor.save()
+        return HttpResponse("Factor created successfully")
