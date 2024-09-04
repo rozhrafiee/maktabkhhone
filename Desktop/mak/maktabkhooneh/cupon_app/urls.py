b7f5c2a22bf9e627168c67750ccdc23496bae438
Pariya.Zarei <art0pariya0z@gmail.com>
2024-09-04 22:18:00 +0330

    adding cupon-app urls.py

--- Desktop/mak/maktabkhooneh/cupon_app/urls.py
+++ Desktop/mak/maktabkhooneh/cupon_app/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from cupon_app.views import cupon_list
-
-urlpatterns = [
-    path('' , cupon_list),
-]
+from django.urls import path from .views import 
+( CuponList, CuponDetail, CreateCupon, UpdateCupon, DeleteCupon, CuponDetailUpdateDelete, )
+urlpatterns = [ path('cupons/', CuponList.as_view(), name='cupon-list'),
+               path('cupons/<int:pk>/', CuponDetail.as_view(), name='cupon-detail'),
+               path('cupons/create/', CreateCupon.as_view(), name='cupon-create'),
+               path('cupons/update/<int:pk>/', UpdateCupon.as_view(), name='cupon-update'), 
+               path('cupons/delete/<int:pk>/', DeleteCupon.as_view(), name='cupon-delete'),
+               path('cupons/manage/<int:pk>/', CuponDetailUpdateDelete.as_view(), name='cupon-manage'), ]
