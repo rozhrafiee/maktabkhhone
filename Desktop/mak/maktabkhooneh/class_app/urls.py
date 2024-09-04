6ac185c09f2dd458de1d9b4786f23ad2da4a5e2e
Pariya.Zarei <art0pariya0z@gmail.com>
2024-09-04 22:21:31 +0330

    adding class_app urls.py

--- Desktop/mak/maktabkhooneh/class_app/urls.py
+++ Desktop/mak/maktabkhooneh/class_app/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
-from class_app.views import class_list , create_class , update_price
-
-urlpatterns = [
-    path('class-lists' , class_list),
-    path('create-clas' , create_class),
-    path('update-price/<str:inp_id>' , update_price)
-]
+from django.urls import path from .views import ClasList, ClasDetail, CreateClas, UpdateClas, DeleteClas, ClasDetailUpdateDelete
+urlpatterns = [ path('', ClasList.as_view(), name='clas_list'),
+               path('<int:pk>/', ClasDetail.as_view(), name='clas_detail'), 
+               path('create/', CreateClas.as_view(), name='create_clas'),
+               path('<int:pk>/update/', UpdateClas.as_view(), name='update_clas'),
+               path('<int:pk>/delete/', DeleteClas.as_view(), name='delete_clas'),
+               path('<int:pk>/detail-update-delete/', ClasDetailUpdateDelete.as_view(), name='clas_detail_update_delete'), ]
