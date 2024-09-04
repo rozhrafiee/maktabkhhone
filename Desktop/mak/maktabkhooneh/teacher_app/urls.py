adf6d73be8dc68ca19731348e31d7d0d8e75a2bf
Pariya.Zarei <art0pariya0z@gmail.com>
2024-09-04 22:23:19 +0330

    adding teacher_app urls.py

--- Desktop/mak/maktabkhooneh/teacher_app/urls.py
+++ Desktop/mak/maktabkhooneh/teacher_app/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path 
-from teacher_app.views import teacher_list , signup , average_rate
-
-urlpatterns = [
-    path("teacher-list" , teacher_list) , 
-    path("signup" , signup) ,
-    path("average-rate" , average_rate) ,
-]
+from django.urls import path from .views import ( TeacherList, TeacherDetail, CreateTeacher, UpdateTeacher, DeleteTeacher,
+TeacherDetailUpdateDelete, AverageRateAPIView, SortByRateAPIView, ) 
+urlpatterns = [ path('', TeacherList.as_view(), name='teacher_list'),
+               path('<int:pk>/', TeacherDetail.as_view(), name='teacher_detail'), 
+               path('create/', CreateTeacher.as_view(), name='create_teacher'),
+               path('<int:pk>/update/', UpdateTeacher.as_view(), name='update_teacher'),
+               path('<int:pk>/delete/', DeleteTeacher.as_view(), name='delete_teacher'),
+               path('detail-update-delete/<int:pk>/', TeacherDetailUpdateDelete.as_view(), name='teacher_detail_update_delete'),
+               path('average-rate/', AverageRateAPIView.as_view(), name='average_rate'),
+               path('sort-by-rate/', SortByRateAPIView.as_view(), name='sort_by_rate'), ]
